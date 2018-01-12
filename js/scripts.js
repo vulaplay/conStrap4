ddecf9b4872cf5f378632eb3a8c350ad7cff2680
paul <pauldavy_home@hotmail.com>
2018-01-12 00:32:45 +0000

    npm scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+            
+    $("#myCarousel").carousel({interval: 2000});
+    
+    $("#carousel-button").click(function(){
+        
+        if ($("#carousel-button").children("span").hasClass('fa fa-pause')) {
+            
+            $("#myCarousel").carousel('pause');
+            $("#carousel-button").children("span").toggleClass('fa-pause fa-play');
+
+        } else {
+
+            $("#myCarousel").carousel('cycle');
+            $("#carousel-button").children("span").toggleClass('fa-play fa-pause');
+        
+        };                
+
+    });
+
+});
